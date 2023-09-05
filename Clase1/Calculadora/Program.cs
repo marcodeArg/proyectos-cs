fc583c288c32cc49a90a9ce93ecb86e3561b4a29
mrcodeArg <marcoemorales211@gmail.com>
2023-09-05 11:08:56 -0300

    Proyectos de la clase 1 - Terminados

---
+++ Clase1/Calculadora/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        try {
+
+            Console.Write("Introduzca el primer número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduzca el segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int suma = num1 + num2;
+            int resta = num1 - num2;
+            int producto = num1 * num2;
+            double cociente = (double)num1 / (double)num2;
+
+            Console.WriteLine($"\nSuma = {suma}\nResta = {resta}\nProducto = {producto}\nCociente = {Math.Round(cociente, 2)}");
+
+        }
+
+        catch(Exception ex) {
+            Console.WriteLine($"\nError: {ex.Message}");
+        }
+
+        
+        
+    }
+}
---
+++ Clase1/Calculadora2/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            string texto = File.ReadAllText("./numeros.csv");
+
+            string[] numeros = texto.Split(",");
+            int indice = 0;
+
+            while(indice < numeros.Length - 1) {
+
+                int num1 = Convert.ToInt32(numeros[indice]);
+                int num2 = Convert.ToInt32(numeros[indice + 1]);
+
+                int suma =  num1 + num2;
+                int resta = num1 - num2;
+                int producto = num1 * num2;
+                double cociente = (double)num1 / (double)num2;
+
+                Console.WriteLine($"\nNumeros {indice+1} y {indice+2}\nSuma = {suma}\nResta = {resta}\nProducto = {producto}\nCociente = {Math.Round(cociente, 2)}");
+
+                indice++;
+            }
+            
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
+    }
+}
