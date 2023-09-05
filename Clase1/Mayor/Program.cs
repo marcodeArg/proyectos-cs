0dea193b0fbf1bc1253fe36f5e488df4f8ea9cc6
mrc <marcoemorales2015@gmail.com>
2023-09-05 00:01:28 -0300

    Proyectos de la clase 1 - Saludo y Mayor terminados

---
+++ Clase1/Mayor/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Mayor de dos números\n");
+
+    Console.Write("Ingrese el primer número: ");
+    int numero1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Ingrese el segundo número: ");
+    int numero2 = Convert.ToInt32(Console.ReadLine());
+
+    int mayor = Math.Max(numero1, numero2);
+    Console.Write($"\nEl mayor es: {mayor}");
+
+
+  }
+}
---
+++ Clase1/Saludo/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.Write("Ingrese su nombre: ");
+    string nombre = Console.ReadLine();
+
+    Console.Write($"Hola {nombre}");
+  }
+}
+
+
