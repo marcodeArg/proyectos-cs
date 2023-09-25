ea4ceac00aedce99b0c56ad85a5eb3afdb4f8d4a
mrc <marcoemorales2015@gmail.com>
2023-09-25 13:10:03 -0300

    Proyectos de la clase 9 - Don quijote y PersonasV2 casi terminados

---
+++ Clase9/PersonasV2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Personas
+{
+  class Program
+  {
+    private static void Main(string[] args)
+    {
+      HashSet<string> nombres = new();
+      HashSet<string> apellidos = new();
+
+
+      using (StreamReader sr = new("./personas.csv"))
+      {
+        while (!sr.EndOfStream)
+        {
+          string[] linea = sr.ReadLine().Split(",");
+
+          nombres.Add(linea[1]);
+          apellidos.Add(linea[2]);
+
+        }
+      };
+
+      Console.WriteLine($"Lista de nombres ({nombres.Count} nombres)");
+      foreach (string nombre in nombres) Console.Write($" {nombre}");
+
+      Console.WriteLine($"\nLista de apellidos ({apellidos.Count} apellidos)");
+      foreach (string apellido in apellidos) Console.Write($" {apellido}");
+    }
+  }
+}
+
---
+++ Clase9/Quijote/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Quijote
+{
+  class Program
+  {
+    private static void Main(string[] args)
+    {
+
+      HashSet<string> palabrasNoRepetidas = new();
+      HashSet<string> palabrasDiccionario = new();
+
+
+      using (StreamReader sr = new("./Quijote.txt"))
+      {
+        while (!sr.EndOfStream)
+        {
+          string[] linea = sr.ReadLine().Split(" ");
+
+          foreach (var palabra in linea)
+          {
+            palabrasNoRepetidas.Add(palabra);
+          }
+        }
+      };
+
+      using (StreamReader sr = new("./words_alpha.txt"))
+      {
+        while (!sr.EndOfStream)
+        {
+          string linea = sr.ReadLine();
+
+          palabrasDiccionario.Add(linea);
+        }
+      };
+
+      SortedSet<string> palabrasOrdenadas = new SortedSet<string>(palabrasNoRepetidas.Except(palabrasDiccionario));
+
+
+      Console.WriteLine($"Cantidad de palabras no repetidas {palabrasNoRepetidas.Count}");
+      Console.WriteLine($"Cantidad de palabras no repetidas {palabrasDiccionario.Count}");
+      Console.WriteLine($" ");
+      Console.WriteLine($" ");
+      Console.WriteLine($"Cantidad de palabras que no existen en el diccionario {palabrasNoRepetidas.Except(palabrasDiccionario).Count()}");
+
+      Console.WriteLine($"Cantidad de palabras que no existen en el diccionario (SortedSet){palabrasOrdenadas.Count}");
+    }
+  }
+}
+
