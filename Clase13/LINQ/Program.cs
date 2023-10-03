4d08de34969548591f66016c7e9b8b4e5ea212fc
mrc <marcoemorales2015@gmail.com>
2023-10-03 00:05:01 -0300

    Archivos de la clase 13

---
+++ Clase13/LINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    List<int> numeros = new();
+    Random r = new Random();
+
+    for (int i = 0; i < 100; i++)
+    {
+      numeros.Add(r.Next(-1000, 1000));
+    }
+
+
+    var esPositivo = (int x) => x > 0;
+    var esNegativo = (int x) => x < 0;
+
+    int numPositivos = numeros.Count(esPositivo);
+    int numNegativos = numeros.Count(x => x > 0);
+    //int numPositivos = numeros.Count(x => x > 0);
+
+    // Mostrar numeros positivos 
+    foreach (var item in numeros.Where(esPositivo))
+    {
+      Console.WriteLine(item);
+    }
+
+  }
+}
